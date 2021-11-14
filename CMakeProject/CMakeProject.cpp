09a428f8b8e23c51b23221070b98b3177f684145
Fabian Töpfer <fabian.toepfer@tu-dresden.de>
2021-11-14 16:17:59 +0100

    initial

---
+++ CMakeProject/CMakeProject.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "CMakeProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
